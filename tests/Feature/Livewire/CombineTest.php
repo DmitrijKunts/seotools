21326696601444d127be04081c4ef859528cf01a
Dmitrij Kunts <r206265@gmail.com>
2022-05-21 18:30:52 +0300

    CombineTest

--- tests/Feature/Livewire/CombineTest.php
+++ tests/Feature/Livewire/CombineTest.php
@@ -10,11 +10,26 @@ use Tests\TestCase;
 
 class CombineTest extends TestCase
 {
+    use WithFaker;
     /** @test */
     public function the_component_can_render()
     {
         $component = Livewire::test(Combine::class);
 
         $component->assertStatus(200);
+    }
+
+    public function test_work_correct()
+    {
+        $list1 = $this->faker->word();
+        $list2 = $this->faker->word();
+        Livewire::test(Combine::class)
+            ->set('list.1', '   ' . $list1 . '   ')
+            ->set('list.2', '   ' . $list2 . '   ')
+            ->call('combine')
+            ->assertSet('result', "$list1 $list2")
+            ->assertNotSet('result', " $list1 $list2")
+            ->assertNotSet('result', $this->faker->word())
+            ->assertSet('resultCount', 1);
     }
 }
