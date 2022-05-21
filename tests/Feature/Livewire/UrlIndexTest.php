d7721cd2df912c0f39e15c7201d3a2ae5ab4d791
Dmitrij Kunts <r206265@gmail.com>
2022-05-21 16:53:23 +0300

    UrlIndexTest

--- tests/Feature/Livewire/UrlIndexTest.php
+++ tests/Feature/Livewire/UrlIndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Livewire;
 
+use App\Http\Livewire\CheckIndex;
 use App\Http\Livewire\UrlIndex;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,11 +11,33 @@ use Tests\TestCase;
 
 class UrlIndexTest extends TestCase
 {
+    use WithFaker;
+
     /** @test */
     public function the_component_can_render()
     {
         $component = Livewire::test(UrlIndex::class);
 
         $component->assertStatus(200);
+    }
+
+    public function test_not_view_url()
+    {
+        $domain = $this->faker->word() . '.' . $this->faker->word();
+        $this->get('/en/url-index/' . $domain)->assertNotFound();
+
+        $domain = 'google.com';
+        Livewire::test(CheckIndex::class)
+        ->set('urls', $domain)
+        ->call('startCheck')
+        ->call('checking')
+        ->assertSee("q=site:$domain");
+    }
+
+    public function test_view_url()
+    {
+        $domain = 'google.com';
+
+        $this->get('/en/url-index/' . $domain)->assertOk();
     }
 }
