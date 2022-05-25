3bfc395978d9cc79acab1795e9838c548e364df8
Dmitrij Kunts <r206265@gmail.com>
2022-05-25 14:14:12 +0300

    test

--- app/Http/Webhooks/SEOToolsHandler.php
+++ app/Http/Webhooks/SEOToolsHandler.php
@@ -9,7 +9,13 @@ class SEOToolsHandler extends WebhookHandler
 {
     public function checkindex()
     {
-        $text = $this->message->text();
-        $this->chat->message("Chat ID:" . $text)->send();
+        Telegraph::message('hello world')
+        ->keyboard(Keyboard::make()->buttons([
+                Button::make('Delete')->action('delete')->param('id', '42'),
+                Button::make('open')->url('https://test.it'),
+        ]))->send();
+        // Telegraph::message('hello world')->send();
+        // $text = $this->message->text();
+        // $this->chat->message("Chat ID:" . $text)->send();
     }
 }
