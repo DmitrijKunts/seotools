63541e833282251f9995341a035e5e174313703e
Dmitrij Kunts <r206265@gmail.com>
2022-05-25 14:21:14 +0300

    bug

--- app/Http/Webhooks/SEOToolsHandler.php
+++ app/Http/Webhooks/SEOToolsHandler.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Webhooks;
 
-use DefStudio\Telegraph\DTO\Message;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
 
 class SEOToolsHandler extends WebhookHandler
 {
     public function checkindex()
     {
         $this->chat->message('hello world')
-        ->keyboard(Keyboard::make()->buttons([
+            ->keyboard(Keyboard::make()->buttons([
                 Button::make('Delete')->action('delete')->param('id', '42'),
                 Button::make('open')->url('https://test.it'),
-        ]))->send();
+            ]))->send();
         // Telegraph::message('hello world')->send();
         // $text = $this->message->text();
         // $this->chat->message("Chat ID:" . $text)->send();
