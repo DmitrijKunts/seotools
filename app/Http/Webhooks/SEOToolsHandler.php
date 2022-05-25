89e401ab08a08fd26866b8e843b15419c9c70bd5
Dmitrij Kunts <r206265@gmail.com>
2022-05-25 15:24:00 +0300

    +

--- app/Http/Webhooks/SEOToolsHandler.php
+++ app/Http/Webhooks/SEOToolsHandler.php
@@ -13,9 +13,9 @@ class SEOToolsHandler extends WebhookHandler
     {
         $this->chat->message('hello world')
             ->keyboard(Keyboard::make()->buttons([
-                Button::make('Check index')->action('/checkindex'),
-                Button::make('Combinator')->action('/combinator'),
-                Button::make('Spintax')->action('/spintax'),
+                Button::make('Check index')->action('/checkindex')->param('id', 'checkindex42'),
+                Button::make('Combinator')->action('/combinator')->param('id', 'combinator142'),
+                Button::make('Spintax')->action('/spintax')->param('id', 'spintax542'),
 
             ]))->send();
     }
