fefd81993aa86ee1982329cbb809b0a263825c79
Dmitrij Kunts <r206265@gmail.com>
2022-05-25 16:04:47 +0300

    1

--- app/Http/Webhooks/SEOToolsHandler.php
+++ app/Http/Webhooks/SEOToolsHandler.php
@@ -5,24 +5,38 @@ namespace App\Http\Webhooks;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use DefStudio\Telegraph\Keyboard\Button;
 use DefStudio\Telegraph\Keyboard\Keyboard;
+use Illuminate\Support\Facades\Cache;
+
+enum TelegraphCmd
+{
+    case CheckIndex;
+    case Combinator;
+    case Spintax;
+}
 
 class SEOToolsHandler extends WebhookHandler
 {
 
-    // protected function handleChatMessage($text): void
-    // {
-    //     $this->chat->message('hello world')
-    //         ->keyboard(Keyboard::make()->buttons([
-    //             Button::make('Check index')->action('checkindex')->param('id', 'checkindex42'),
-    //             Button::make('Combinator')->action('combinator')->param('id', 'combinator1'),
-    //             Button::make('Spintax')->action('spintax')->param('id', 'spintax542'),
+    private function genKey()
+    {
+        return 'telegraph:lastcmd:' . $this->chat->chat_id;
+    }
 
-    //         ]))->send();
-    // }
+    protected function handleChatMessage($text): void
+    {
+        if ($cmd = Cache::get($this->genKey())) {
+            if ($cmd == TelegraphCmd::CheckIndex) {
+                $this->chat->message('Checking index for ' . $text)->send();
+                return;
+            }
+        }
+        $this->chat->message('Select command')->send();
+    }
 
     public function checkindex()
     {
-        $this->chat->message("checkindex: dddddddddddd")->send();
+        Cache::put($this->genKey(), TelegraphCmd::CheckIndex, 60 * 15);
+        $this->chat->reply('Urls one per line');
     }
 
     public function combinator()
