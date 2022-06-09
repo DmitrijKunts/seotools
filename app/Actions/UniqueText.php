8b5cce01e13c90e2f8d46ca582c3b787566059ee
Dmitrij Kunts <r206265@gmail.com>
2022-06-09 10:02:29 +0300

    Action complete

--- app/Actions/UniqueText.php
+++ app/Actions/UniqueText.php
@@ -2,6 +2,40 @@
 
 namespace App\Actions;
 
+use \Statickidz\GoogleTranslate;
+
 class UniqueText
 {
+    public static function unique($text, $ln = 'ru')
+    {
+        $trans = new GoogleTranslate();
+
+        //https://cloud.google.com/translate/docs/languages
+        $allowLangs = [
+            'ru',
+            'en',
+            'fr',
+            'de',
+            'el',
+            'ga',
+            'pl',
+            'pt',
+            'sv',
+            'it',
+            'fi',
+            'da',
+        ];
+
+        #Tier 1
+        $allowLangs = array_diff($allowLangs, [$ln]);
+        $targetTier1 = $allowLangs[array_rand($allowLangs)];
+        $text = $trans->translate($ln, $targetTier1, $text);
+
+        #Tier 2
+        $allowLangs = array_diff($allowLangs, [$targetTier1]);
+        $targetTier2 = $allowLangs[array_rand($allowLangs)];
+        $text = $trans->translate($targetTier1, $targetTier2, $text);
+
+        return $trans->translate($targetTier2, $ln, $text);
+    }
 }
